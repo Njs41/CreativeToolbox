c1e30c8aca5181cad617f4b70ed38747b3682107
njs41 <njs42@hotmail.com>
2017-05-15 15:41:15 -0400

    Store players in plot lists using their UUID instead of username.

--- src/no/runsafe/creativetoolbox/PlotList.java
+++ src/no/runsafe/creativetoolbox/PlotList.java
@@ -6,33 +6,34 @@ import no.runsafe.framework.api.player.IPlayer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PlotList
 {
 	public void set(IPlayer player, List<String> list)
 	{
-		lists.put(player.getName(), list);
+		lists.put(player.getUniqueId(), list);
 	}
 
 	public int current(IPlayer player)
 	{
-		return lists.get(player.getName()).indexOf(pointer.get(player.getName())) + 1;
+		return lists.get(player.getUniqueId()).indexOf(pointer.get(player.getUniqueId())) + 1;
 	}
 
 	public void wind(IPlayer player, String to)
 	{
-		pointer.put(player.getName(), to);
+		pointer.put(player.getUniqueId(), to);
 	}
 
 	public int count(IPlayer player)
 	{
-		return lists.get(player.getName()).size();
+		return lists.get(player.getUniqueId()).size();
 	}
 
 	public void remove(String plot)
 	{
-		for (Map.Entry<String, List<String>> list : lists.entrySet())
+		for (Map.Entry<UUID, List<String>> list : lists.entrySet())
 		{
 			if (list.getValue().contains(plot))
 			{
@@ -58,32 +59,32 @@ public class PlotList
 
 	public String previous(IPlayer player)
 	{
-		if (lists.containsKey(player.getName()))
+		if (lists.containsKey(player.getUniqueId()))
 		{
-			List<String> list = lists.get(player.getName());
+			List<String> list = lists.get(player.getUniqueId());
 			if (list == null || list.isEmpty())
 				return null;
-			int i = list.indexOf(pointer.get(player.getName()));
-			pointer.put(player.getName(), list.get(i > 0 ? i - 1 : list.size() - 1));
-			return pointer.get(player.getName());
+			int i = list.indexOf(pointer.get(player.getUniqueId()));
+			pointer.put(player.getUniqueId(), list.get(i > 0 ? i - 1 : list.size() - 1));
+			return pointer.get(player.getUniqueId());
 		}
 		return null;
 	}
 
 	public String next(IPlayer player)
 	{
-		if (lists.containsKey(player.getName()))
+		if (lists.containsKey(player.getUniqueId()))
 		{
-			List<String> list = lists.get(player.getName());
+			List<String> list = lists.get(player.getUniqueId());
 			if (list == null || list.isEmpty())
 				return null;
-			int i = list.indexOf(pointer.get(player.getName()));
-			pointer.put(player.getName(), list.get(i + 1 >= list.size() ? 0 : i + 1));
-			return pointer.get(player.getName());
+			int i = list.indexOf(pointer.get(player.getUniqueId()));
+			pointer.put(player.getUniqueId(), list.get(i + 1 >= list.size() ? 0 : i + 1));
+			return pointer.get(player.getUniqueId());
 		}
 		return null;
 	}
 
-	private final ConcurrentHashMap<String, String> pointer = new ConcurrentHashMap<String, String>();
-	private final ConcurrentHashMap<String, List<String>> lists = new ConcurrentHashMap<String, List<String>>();
+	private final ConcurrentHashMap<UUID, String> pointer = new ConcurrentHashMap<UUID, String>();
+	private final ConcurrentHashMap<UUID, List<String>> lists = new ConcurrentHashMap<UUID, List<String>>();
 }
